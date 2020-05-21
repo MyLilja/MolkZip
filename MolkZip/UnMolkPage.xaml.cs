d948367e028803e5970593afa4599c13bd1666a0
Eddie Mossberg <eveleddie00rgmail.com>
2020-05-21 12:01:54 +0200

    Made it work!

--- MolkZip/UnMolkPage.xaml.cs
+++ MolkZip/UnMolkPage.xaml.cs
@@ -7,7 +7,8 @@ using TextBox = System.Windows.Controls.TextBox;
 using ToolTip = System.Windows.Controls.ToolTip;
 using System.Diagnostics;
 using System.Text.RegularExpressions;
-
+using System.Collections.Generic;
+using System;
 
 namespace MolkZip
 {
@@ -43,9 +44,10 @@ namespace MolkZip
             filepath.Filter = "Molk Files|*.molk";
             DialogResult result = filepath.ShowDialog();
             Path_Name.Text = filepath.FileName;
-
+            //It works! I don't know how and i don't question it!
             if (result == WinForms.DialogResult.OK)
             {
+                listFiles.Items.Clear();
                 Process proc = new Process();
 
                 proc.StartInfo.FileName = "cmd.exe";
@@ -74,7 +76,6 @@ namespace MolkZip
                         {
                             closLin = lin.Index;
                             listFiles.Items.Add(text.Substring(groups[1].Index+6, (closLin - groups[1].Index-6)));
-                            // { groups[1].Index} { closLin}
                             break;
                         } 
                     }  
@@ -101,6 +102,8 @@ namespace MolkZip
                 browseText.Opacity = 0;
                 arrowText.Opacity = 0;
                 exitText.Opacity = 0;
+                removeFiles.Opacity = 0;
+                addFiles.Opacity = 0;
             }
             else
             {
@@ -108,6 +111,8 @@ namespace MolkZip
                 browseText.Opacity = 1;
                 arrowText.Opacity = 1;
                 exitText.Opacity = 1;
+                removeFiles.Opacity = 1;
+                addFiles.Opacity = 1;
             }
 
         }
@@ -123,5 +128,75 @@ namespace MolkZip
             Exit3.BeginStoryboard(story);
 
         }
+
+        private void remove_files(object sender, RoutedEventArgs e)
+        {
+            List<string> files = new List<string>();
+            try
+            {
+                foreach (string item in Choosen_files.SelectedItems)
+                {
+                    files.Add(item);
+                }
+
+                for (int i = 0; i < Choosen_files.SelectedItems.Count + 1; i++)
+                {
+                    Choosen_files.Items.Remove(files[i]);
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Windows.MessageBox.Show("Invalid Operation. " + ex.Message, "No files to remove from this list.", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void add_files(object sender, RoutedEventArgs e)
+        {
+            foreach (string item in listFiles.SelectedItems)
+            {
+                for (int i = 0; i < Choosen_files.Items.Count + 1; i++)
+                {
+                    if (!Choosen_files.Items.Contains(item))
+                    {
+                        Choosen_files.Items.Add(item);
+                    }
+                }
+            }
+        }
+
+         private void Unmolk(object sender, RoutedEventArgs e)
+         {
+            FolderBrowserDialog target = new FolderBrowserDialog();
+            target.ShowNewFolderButton = false;
+            target.SelectedPath = System.AppDomain.CurrentDomain.BaseDirectory;
+            target.ShowDialog();
+            Process proc = new Process();
+
+             proc.StartInfo.FileName = "cmd.exe";
+             proc.StartInfo.RedirectStandardInput = true;
+             proc.StartInfo.RedirectStandardOutput = true;
+             proc.StartInfo.RedirectStandardError = true;
+             proc.StartInfo.CreateNoWindow = true;
+             proc.StartInfo.UseShellExecute = false;
+
+             string command = "unmolk -o " + Path_Name.Text + " ";
+             foreach (string item in Choosen_files.Items)
+             {
+                 command += item + " ";
+             }
+             bool once = true;
+             while (once)
+             {
+
+                 proc.Start();
+                WinForms.MessageBox.Show($"{command}-d " + target.SelectedPath);
+                 proc.StandardInput.WriteLine($"{command}-d " + target.SelectedPath);
+                 proc.StandardInput.Flush();
+                 proc.StandardInput.Close();
+
+                 proc.WaitForExit();
+                 once = false;
+             }
+         }
     }
 }
